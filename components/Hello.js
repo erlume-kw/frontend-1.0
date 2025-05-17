4b7cb83991a6f4ce79e7116a9da768aef2ecc60b
Janna <jannakalmuqaisib@gmail.com>
2025-05-17 18:11:00 +0300

    testing web hook

--- components/Hello.js
+++ components/Hello.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Hello = () => {
   return (
     <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-2xl font-bold'>Hello</Text>
+      <Text className='text-2xl font-bold'>Hello World</Text>
     </View>
   )
 }
